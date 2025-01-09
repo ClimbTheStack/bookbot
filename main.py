59aadf2c3b7122c79009c59c23e22b6de5deaa8b
Djolebest <jonathanroy@me.com>
2025-01-09 07:22:50 -0500

    final

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     characters = get_character_dict(text)
+    print(f"--- Begin report of {book_path} ---")
     print(f"{num_words} words found in the document")
-    print(characters)
+    sorted_list = print_report(characters, num_words)
+
+    # Then in your main code:
+    #word_count = get_word_count()  # or however you're getting this
+    #char_freq = get_char_frequency()  # or however you're getting this
+    #print_report(char_freq, word_count)
+
+
 
 def get_num_words(text):
     words = text.split()
@@ -26,6 +34,17 @@ def get_character_dict(text):
             dict[letter] = 1
     return dict
 
+def print_report(char_freq, word_count):
+    list = []
+    for letter, count in char_freq.items():
+        if letter.isalpha():
+            dict = {"char": letter, "num" : count}
+            list.append(dict)
 
+    def sort_on(dict):
+        return dict["num"]
 
+    list.sort(reverse = True, key = sort_on)
+    for letters in list:
+        print(f"The '{letters['char']}' character was found {letters['num']} times")
 main()
